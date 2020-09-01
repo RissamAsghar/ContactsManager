7f1458a90d0f0e699de52e942e79856b6cfbe93a
RissamAsghar <rissam03oct@gmail.com>
2020-09-01 21:22:08 +0500

    Fixing Loop

--- Main.java
+++ Main.java
@@ -2,30 +2,41 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String [] args){
     
-    ContactsManager myContactsManager = new ContactsManager();
+    static Scanner input = new Scanner(System.in);
+    private static ContactsManager myContactsManager = new ContactsManager();
+    private static int options;
 
-     //Print the options for the user to choose from
-		System.out.println("*****Welcome to Contacts Manager*****");
+    public static int start(){
+
+        System.out.println("*****Welcome to Contacts Manager*****");
 		System.out.println("*. Press 1 to create contact");
 		System.out.println("*. Press 2 to search contact");
         System.out.println("*. Press 3 to exit");
-   
 
-    Scanner input = null;
-
-    try{
-
-        input = new Scanner(System.in);
- 
         int options = input.nextInt();
 
-        while (input.hasNextLine()){
+        while (options < 1 || options > 3){
 
-        switch(options){
-            case 1:
+            System.out.println ("that is an invalid select.");
+            System.out.println (" Enter 1, 2, or 3s");
+            options = input.nextInt();
+    }
 
+    return options;
+       
+
+    }
+
+    public static void searchContact(){
+                System.out.println("Search Contact: ");
+                String searchName = input.next();
+                Contact result = myContactsManager.searchContact(searchName);
+                System.out.println("The phone number is: " +result.getPhonenumber());
+
+    }
+
+    public static void createContact(){
                 System.out.println("Enter the name of the contact: ");
                 String name = input.next();
                 Contact Rissam = new Contact();
@@ -37,32 +48,37 @@ public class Main {
                 Rissam.setPhoneNumber(phoneNumber);
 
                 myContactsManager.addContact(Rissam);
-                
+                System.out.println("Contact added successfully!!");
+
+    }
+
+    public static void main(String [] args){
+    
+
+
+        do{
+            options = start();
+           
+        switch(options){
+            case 1:
+
+                createContact();
+                options = 0;
                 break;
 
             case 2:
-                System.out.println("Enter the name of the contact: ");
-                String searchName = input.nextLine();
-                Contact result = myContactsManager.searchContact(searchName);
-                System.out.println("The phone number is: " +result.getPhonenumber());
+
+                searchContact();
+                options = 0;
                 break;
-            case 3:
-                input.close();
-                System.exit(0);
-                break;    
+
+
         }
     }
+
+    while(options !=3);
+
             
-    
-    
     }
-    
-    finally{
-        
-        if(input!=null)
-        input.close();
-    }
-    
-}
     
 }
